21e2bf45379e4e533d0b1bbfb4a4431128d7e890
hatem-nefzi <hatem.nefzibouali@ensi-uma.tn>
2025-07-28 13:51:55 +0100

    fixing unit tests

--- karma.conf.js
+++ karma.conf.js
@@ -1,21 +1,34 @@
 module.exports = function (config) {
   config.set({
-    // ...
+    basePath: '',
+    frameworks: ['jasmine', '@angular-devkit/build-angular'],
+    plugins: [
+      require('karma-jasmine'),
+      require('karma-junit-reporter'),
+      require('karma-coverage'),
+      require('karma-chrome-launcher'),
+      require('@angular-devkit/build-angular/plugins/karma')
+    ],
+    client: {
+      clearContext: false // leave Jasmine Spec Runner output visible in browser
+    },
     reporters: ['progress', 'junit', 'coverage'],
-
     junitReporter: {
-      outputDir: 'coverage',
+      outputDir: '.',
       outputFile: 'junit.xml',
       useBrowserName: false
     },
-
     coverageReporter: {
+      dir: require('path').join(__dirname, './coverage'),
+      subdir: '.',
       reporters: [
-        { type: 'html', dir: 'coverage/' },
-        { type: 'cobertura', file: 'cobertura-coverage.xml' }
+        { type: 'html' },
+        { type: 'text-summary' },
+        { type: 'cobertura' }
       ]
     },
-
-    // ...
+    browsers: ['ChromeHeadless'],
+    singleRun: false,
+    restartOnFileChange: true
   });
 };
